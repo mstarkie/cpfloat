d979ce28c730aabd44b8e0fe27b5266a49edc96e
Mike Starkie <hikemike@gmail.com>
2016-08-14 09:43:18 -0400

    Example shows a 32 bit integer holding an IEEE 754 32 bit single precision floating point number.

--- cpfloat/main.cpp
+++ cpfloat/main.cpp
@@ -8,8 +8,16 @@
 
 #include <iostream>
 
+#define asreal(x) (*((float *) &x))
+
+typedef long unsigned real;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    real l;
+    //float t = *( (*float) const &1);
+    asreal(l) = 1.0;
+    printf("l as hex     = %lx\n", l);
+    printf("l as real    = %f\n", asreal(l));
+    printf("l as decimal = %lu\n", l);
     return 0;
 }
