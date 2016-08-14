41fd99ed4661011bd0e025b575e7eb4400a996d6
Mike Starkie <hikemike@gmail.com>
2016-08-14 09:06:09 -0400

    Initial Commit

---
+++ cpfloat/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpfloat
+//
+//  Created by Michael Starkie on 8/14/16.
+//  Copyright © 2016 Global Forge, LLC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
